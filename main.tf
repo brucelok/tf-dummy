dfe82a90bfb20776f6737a6b07990e87aa2c6f8c
Bruce L. <2253713+brucelok@users.noreply.github.com>
2025-03-28 14:39:28 +1100

    print env var

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-resource "null_resource" "simulate_workload" {
-  count = 60
+#resource "null_resource" "simulate_workload" {
+#  count = 60
+#
+#  provisioner "local-exec" {
+#    command = "sleep 10"
+#  }
+#}
 
+#output "simulation_complete" {
+#  value = "Done simulating ${length(null_resource.simulate_workload)} resources!"
+#}
+
+resource "null_resource" "print_env" {
   provisioner "local-exec" {
-    command = "sleep 60"
+    command = "env"
   }
-}
-
-output "simulation_complete" {
-  value = "Done simulating ${length(null_resource.simulate_workload)} resources!"
 }

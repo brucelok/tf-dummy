347ed0b474a10e550dcf9e8f4a8fb4e98983ae21
Bruce L. <2253713+brucelok@users.noreply.github.com>
2025-10-23 11:00:14 +1100

    Add null resource to print environment variables
    Added a null resource to print environment variables.

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "null_resource" "print_env" {
+  provisioner "local-exec" {
+    command = "env"
+  }
+}
+
 resource "null_resource" "simulate_workload" {
   provisioner "local-exec" {
     command = "sleep 3600"
@@ -7,9 +13,3 @@ resource "null_resource" "simulate_workload" {
 #output "simulation_complete" {
 #  value = "Done simulating ${length(null_resource.simulate_workload)} resources!"
 #}
-
-resource "null_resource" "print_env" {
-  provisioner "local-exec" {
-    command = "env"
-  }
-}
